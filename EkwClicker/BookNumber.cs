ef440aff51622ef5cec3d870e083dc47d21d335a
maszrum <grzyb3k@hotmail.com>
2020-09-10 20:13:15 +0200

    Changed BookNumber parameters to be initialized from constructor

--- EkwClicker/BookNumber.cs
+++ EkwClicker/BookNumber.cs
@@ -1,9 +1,40 @@
-﻿namespace EkwClicker
+﻿using System;
+using System.Linq;
+
+namespace EkwClicker
 {
     internal class BookNumber
     {
-        public string CourtCode { get; set; }
-        public string Number { get; set; }
-        public int ControlNumber { get; set; }
+        public BookNumber(string courtCode, string number)
+        {
+            if (courtCode.Length != 4)
+            {
+                throw new ArgumentException(
+                    "must have exactly 4 characters", nameof(courtCode));
+            }
+            if (number.Length != 8 || number.Any(c => !char.IsDigit(c)))
+            {
+                throw new ArgumentException(
+                    "must have 8 digits", nameof(number));
+            }
+            
+            CourtCode = courtCode;
+            Number = number;
+        }
+        
+        public BookNumber(string courtCode, string number, int controlNumber) 
+            : this(courtCode, number)
+        {
+            ControlNumber = controlNumber;
+        }
+        
+        public string CourtCode { get; }
+        public string Number { get; }
+        public int? ControlNumber { get; private set; }
+        
+        public void SetControlNumber(int value)
+        {
+            ControlNumber = value;
+        }
     }
 }
