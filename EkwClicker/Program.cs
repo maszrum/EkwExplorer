d885b9baaa3c42fabfcc025803b1a7eb240906e4
maszrum <grzyb3k@hotmail.com>
2020-09-10 10:30:20 +0200

    Added SeleniumClicker class

--- EkwClicker/Program.cs
+++ EkwClicker/Program.cs
@@ -1,13 +1,39 @@
 ﻿using System;
-using OpenQA.Selenium.Chrome;
+using System.Threading.Tasks;
 
 namespace EkwClicker
 {
 	internal class Program
 	{
-		static void Main(string[] args)
+		private static async Task Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			// wyszukaj
+
+			var url = "https://przegladarka-ekw.ms.gov.pl/eukw_prz/KsiegiWieczyste/wyszukiwanieKW";
+			using var clicker = new SeleniumClicker(url);
+
+			await Task.Delay(1000);
+
+			clicker.CloseCookiesInfo("//*[@id=\"cookies\"]/div/span/span");
+
+			clicker.FillTextbox("kodWydzialuInput", "NS1T");
+			clicker.FillTextbox("numerKsiegiWieczystej", "00046573");
+			clicker.FillTextbox("cyfraKontrolna", "5");
+
+			clicker.ClickButton("wyszukaj");
+
+			if (clicker.CheckIfAnyError())
+			{
+				throw new Exception("captcha error");
+			}
+
+			string bookType = clicker.GetValueFromTable("Typ księgi wieczystej");
+			string openingDate = clicker.GetValueFromTable("Data zapisania księgi wieczystej");
+			string closureDate = clicker.GetValueFromTable("Data zamknięcia księgi wieczystej");
+			string location = clicker.GetValueFromTable("Położenie");
+			string owner = clicker.GetValueFromTable("Właściciel");
+
+			clicker.ClickButton("powrotDoKryterii");
 		}
 	}
 }
---
+++ EkwClicker/SeleniumClicker.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+
+namespace EkwClicker
+{
+	internal class SeleniumClicker : IDisposable
+	{
+		private readonly ChromeDriver _driver;
+
+		public SeleniumClicker(string url)
+		{
+			_driver = new ChromeDriver();
+			_driver.Navigate().GoToUrl(url);
+		}
+
+		public void FillTextbox(string textboxId, string text)
+		{
+			_driver
+				.FindElement(By.Id(textboxId))
+				.SendKeys(text);
+		}
+
+		public void ClickButton(string buttonId)
+		{
+			var button = _driver.FindElement(By.Id(buttonId));
+			var actions = new Actions(_driver);
+
+			actions
+				.MoveToElement(button)
+				.Click()
+				.Perform();
+		}
+
+		public string GetValueFromTable(string rowCaption)
+		{
+			var elements = _driver.FindElements(By.ClassName("form-row"));
+
+			foreach (var element in elements)
+			{
+				var captionDiv = GetElementOrDefault(element, By.ClassName("label-column-50"));
+				if (captionDiv == null)
+					continue;
+
+				var captionLabel = GetElementOrDefault(captionDiv, By.TagName("label"));
+				if (captionLabel == null)
+					continue;
+
+				if (captionLabel.Text.Contains(rowCaption))
+				{
+					var contentDiv = element.FindElement(By.ClassName("content-column-50"));
+					var contentDivInner = contentDiv.FindElement(By.TagName("div"));
+
+					return contentDivInner.Text;
+				}
+			}
+
+			return null;
+		}
+
+		public bool CheckIfAnyError()
+		{
+			var container = _driver
+				.FindElements(By.Id("recaptchaResponseErrors"))
+				.FirstOrDefault();
+
+			if (container == default)
+			{
+				return false;
+			}
+
+			var errorsElements = container.FindElements(By.TagName("span"));
+
+			return errorsElements.Count > 0;
+		}
+
+		internal void CloseCookiesInfo(string xPath)
+		{
+			_driver
+				.FindElement(By.XPath(xPath))
+				.Click();
+		}
+
+		public void Dispose()
+		{
+			_driver.Dispose();
+		}
+
+		private static IWebElement GetElementOrDefault(IWebElement element, By by)
+			=> element.FindElements(by).FirstOrDefault();
+	}
+}
