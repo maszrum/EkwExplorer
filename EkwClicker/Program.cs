2038b6cdd17e5040a91cb554c8e85dac71f994e4
maszrum <grzyb3k@hotmail.com>
2020-09-10 08:55:38 +0200

    Created project

---
+++ EkwClicker/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+
+namespace EkwClicker
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
