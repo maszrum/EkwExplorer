001efd747ab619b52b3d4d3ee0edd6b105a6e662
maszrum <grzyb3k@hotmail.com>
2020-10-22 20:17:27 +0200

    some program.cs improvments, still under construction

--- EkwClicker/Program.cs
+++ EkwClicker/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using EkwClicker.Algorithms;
+using EkwClicker.Core;
 using EkwClicker.Datasource;
 using EkwClicker.Datasource.Repositories;
 using EkwClicker.Models;
@@ -12,6 +13,9 @@ namespace EkwClicker
 {
     internal class Program
     {
+        private static readonly Random Random = new Random(DateTime.Now.Millisecond);
+        private static readonly ChromeOptionsProvider OptionsProvider = new ChromeOptionsProvider();
+        
         private static async Task Main(string[] args)
         {
             var availableDatabases = DbAccess.GetAvailableDatabases();
@@ -39,49 +43,106 @@ namespace EkwClicker
                 await seeder.SeedAsync(numberFrom, numberTo);
             }
 
-            using var clicker = new SeleniumClicker();
-            var seeker = new BookInfoSeeker(clicker);
-
-            clicker.GotoHome();
-            await Task.Delay(1000);
-            clicker.CloseCookiesInfo();
-
+            var seeker = await OpenSeeker();
+            
+            var captchaErrors = 0;
+            var downloadedBooks = 0;
+            
             while (true)
             {
-                var randomBook = await repository.GetRandomNotFilledBookAsync();
-
-                var bookExists = false;
-                var captchaError = false;
                 try
                 {
-                    bookExists = seeker.ReadBookInfo(randomBook);
+                    await ExploringStep(seeker, repository);
+                    downloadedBooks++;
+                    captchaErrors = 0;
                 }
                 catch (Exception exception) when (exception.Message.Contains("captcha"))
                 {
-                    captchaError = true;
+                    captchaErrors++;
+                    if (captchaErrors > 5)
+                    {
+                        captchaErrors = 0;
+                        
+                        Console.WriteLine("Reopening chrome driver...");
+                        seeker = await ReopenSeeker(seeker);
+                        Console.WriteLine("Opened new istance of chrome driver");
+                    }
+                }
+                catch (Exception ex) when (ex.GetType().Namespace == "OpenQA.Selenium")
+                {
+                    seeker = await ReopenSeeker(seeker);
+                }
+                catch (InvalidOperationException ioe) when (ioe.Message.Contains("was not read"))
+                {
+                    seeker = await ReopenSeeker(seeker);
                 }
                 
-                if (!captchaError)
-                {
-                    if (!bookExists)
-                    {
-                        BookInfo.MarkAsNotFound(randomBook);
-                    }
-                    else
-                    {
-                        if (randomBook.ClosureDate.Length <= 4)
-                        {
-                            var properties = seeker.ReadProperties();
-                            randomBook.AddNewProperties(properties);
-                        }
-                    }
-
-                    await repository.UpdateBookAsync(randomBook);
-                    await repository.AddPropertyFromBookAsync(randomBook);
-                    
-                    clicker.BackToCriteria();
-                }
+                Console.WriteLine($"Downloaded books: {downloadedBooks}");
             }
         }
+        
+        private static Task<BookInfoSeeker> OpenSeeker()
+            => ReopenSeeker(null);
+        
+        private static async Task<BookInfoSeeker> ReopenSeeker(BookInfoSeeker seeker)
+        {
+            seeker?.Clicker.Dispose();
+            
+            var clicker = await CreateClicker(OptionsProvider);
+            
+            seeker = new BookInfoSeeker(clicker);
+            return seeker;
+        }
+        
+        private static async Task<IClicker> CreateClicker(ChromeOptionsProvider optionsProvider)
+        {
+            var clicker = new SeleniumClicker(optionsProvider.Get());
+            
+            clicker.GotoHome();
+            await Task.Delay(1000);
+            clicker.CloseCookiesInfo();
+            
+            return clicker;
+        }
+        
+        private static async Task ExploringStep(BookInfoSeeker seeker, IBooksRepository repository)
+        {
+            await Task.Delay(GetRandomDelay());
+            
+            var randomBook = await repository.GetRandomNotFilledBookAsync();
+            
+            await Task.Delay(GetRandomDelay());
+            
+            var bookExists = seeker.ReadBookInfo(randomBook);
+            
+            if (!bookExists)
+            {
+                BookInfo.MarkAsNotFound(randomBook);
+            }
+            else
+            {
+                if (randomBook?.BookType == null)
+                {
+                    throw new InvalidOperationException(
+                        "something went wrong, book properties was not read");
+                }
+                
+                if (randomBook.ClosureDate.Length <= 4)
+                {
+                    var properties = seeker.ReadProperties();
+                    randomBook.AddNewProperties(properties);
+                }
+            }
+
+            await repository.UpdateBookAsync(randomBook);
+            await repository.AddPropertyFromBookAsync(randomBook);
+                    
+            await Task.Delay(GetRandomDelay());
+
+            seeker.BackToCriteria();
+        }
+        
+        private static int GetRandomDelay() 
+            => Random.Next(800, 2000);
     }
 }
