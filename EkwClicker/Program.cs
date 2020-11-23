09d2445ba7c53494d191a2fb924a4aca9f61a726
maszrum <grzyb3k@hotmail.com>
2020-11-23 22:00:53 +0100

    extracted most of code to BooksExplorer class & added logging

--- EkwClicker/Program.cs
+++ EkwClicker/Program.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using EkwClicker.Algorithms;
-using EkwClicker.Core;
 using EkwClicker.Datasource;
 using EkwClicker.Datasource.Repositories;
 using EkwClicker.Models;
 using EkwClicker.Seeker;
+using Serilog;
 
 // ReSharper disable ClassNeverInstantiated.Global
 
@@ -14,64 +14,41 @@ namespace EkwClicker
 {
     internal class Program
     {
-        private static readonly Random Random = new Random(DateTime.Now.Millisecond);
-        private static readonly ChromeOptionsProvider OptionsProvider = new ChromeOptionsProvider();
-        
         private static async Task Main(string[] args)
         {
+            var logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .CreateLogger()
+                .ForContext<Program>();
+            
             var input = ReadProgramInput(args);
-            Console.WriteLine(input);
+            
+            var inputLines = input.ToString().Split(Environment.NewLine);
+            foreach(var inputLine in inputLines)
+            {
+                logger.Information(inputLine);
+            }
 
             await using var connection = DbAccess.Exists(input.DatabaseFile)
                 ? await DbAccess.Connect(input.DatabaseFile)
                 : await DbAccess.Create(input.DatabaseFile);
 
             var repository = new BooksRepository(connection);
-            var seeder = new DatasourceSeeder(repository);
 
             if (input.NumberFrom.HasValue && input.NumberTo.HasValue)
             {
                 var numberFrom = new BookNumber(input.CourtCode, input.NumberFrom.Value.ToString("D8"));
                 var numberTo = new BookNumber(input.CourtCode, input.NumberTo.Value.ToString("D8"));
+                
+                var seeder = new DatasourceSeeder(repository);
                 await seeder.SeedAsync(numberFrom, numberTo);
             }
-
-            var seeker = await OpenSeeker();
             
-            var captchaErrors = 0;
-            var downloadedBooks = 0;
+            var explorer = new BooksExplorer(logger, repository);
             
-            while (true)
-            {
-                try
-                {
-                    await ExploringStep(seeker, repository);
-                    downloadedBooks++;
-                    captchaErrors = 0;
-                }
-                catch (Exception exception) when (exception.Message.Contains("captcha"))
-                {
-                    captchaErrors++;
-                    if (captchaErrors > 5)
-                    {
-                        captchaErrors = 0;
-                        
-                        Console.WriteLine("Reopening chrome driver...");
-                        seeker = await ReopenSeeker(seeker);
-                        Console.WriteLine("Opened new istance of chrome driver");
-                    }
-                }
-                catch (Exception ex) when (ex.GetType().Namespace == "OpenQA.Selenium")
-                {
-                    seeker = await ReopenSeeker(seeker);
-                }
-                catch (InvalidOperationException ioe) when (ioe.Message.Contains("was not read"))
-                {
-                    seeker = await ReopenSeeker(seeker);
-                }
-                
-                Console.WriteLine($"Downloaded books: {downloadedBooks}");
-            }
+            await explorer.Open();
+            await explorer.Explore();
         }
         
         private static ProgramInput ReadProgramInput(IReadOnlyList<string> args)
@@ -97,69 +74,5 @@ namespace EkwClicker
                     return ProgramInput.ReadFromArgs(args);
             }
         }
-        
-        private static Task<BookInfoSeeker> OpenSeeker()
-            => ReopenSeeker(null);
-        
-        private static async Task<BookInfoSeeker> ReopenSeeker(BookInfoSeeker seeker)
-        {
-            seeker?.Clicker.Dispose();
-            
-            var clicker = await CreateClicker(OptionsProvider);
-            
-            seeker = new BookInfoSeeker(clicker);
-            return seeker;
-        }
-        
-        private static async Task<IClicker> CreateClicker(ChromeOptionsProvider optionsProvider)
-        {
-            var clicker = new SeleniumClicker(optionsProvider.Get());
-            
-            clicker.GotoHome();
-            await Task.Delay(1000);
-            clicker.CloseCookiesInfo();
-            
-            return clicker;
-        }
-        
-        private static async Task ExploringStep(BookInfoSeeker seeker, IBooksRepository repository)
-        {
-            await Task.Delay(GetRandomDelay());
-            
-            var randomBook = await repository.GetRandomNotFilledBookAsync();
-            
-            await Task.Delay(GetRandomDelay());
-            
-            var bookExists = seeker.ReadBookInfo(randomBook);
-            
-            if (!bookExists)
-            {
-                BookInfo.MarkAsNotFound(randomBook);
-            }
-            else
-            {
-                if (randomBook?.BookType == null)
-                {
-                    throw new InvalidOperationException(
-                        "something went wrong, book properties was not read");
-                }
-                
-                if (randomBook.ClosureDate.Length <= 4)
-                {
-                    var properties = seeker.ReadProperties();
-                    randomBook.AddNewProperties(properties);
-                }
-            }
-
-            await repository.UpdateBookAsync(randomBook);
-            await repository.AddPropertyFromBookAsync(randomBook);
-                    
-            await Task.Delay(GetRandomDelay());
-
-            seeker.BackToCriteria();
-        }
-        
-        private static int GetRandomDelay() 
-            => Random.Next(800, 2000);
     }
 }
---
+++ EkwClicker/Seeker/BooksExplorer.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Threading.Tasks;
+using EkwClicker.Core;
+using EkwClicker.Models;
+using Serilog;
+
+namespace EkwClicker.Seeker
+{
+    internal class BooksExplorer
+    {
+        private static readonly ChromeOptionsProvider ChromeOptions = new ChromeOptionsProvider();
+        
+        private readonly ILogger _logger;
+        private readonly IBooksRepository _booksRepository;
+        
+        private readonly Random _random = new Random(DateTime.Now.Millisecond);
+        
+        private BookInfoSeeker _seeker;
+        
+        public BooksExplorer(ILogger logger, IBooksRepository booksRepository)
+        {
+            _logger = logger;
+            _booksRepository = booksRepository;
+        }
+        
+        public async Task Open()
+        {
+            _seeker = await OpenSeeker();
+        }
+        
+        public async Task Explore()
+        {
+            var captchaErrors = 0;
+            var downloadedBooks = 0;
+            
+            while (true)
+            {
+                try
+                {
+                    await ExploringStep();
+                    downloadedBooks++;
+                    captchaErrors = 0;
+                }
+                catch (Exception exception) when (exception.Message.Contains("captcha"))
+                {
+                    _logger.Warning("Detected captcha.");
+                    
+                    captchaErrors++;
+                    if (captchaErrors > 5)
+                    {
+                        captchaErrors = 0;
+                        
+                        _logger.Information("Reopening chrome driver...");
+                        _seeker = await ReopenSeeker(_seeker);
+                        _logger.Information("Opened new istance of chrome driver");
+                    }
+                }
+                catch (Exception ex) when (ex.GetType().Namespace == "OpenQA.Selenium")
+                {
+                    _logger.Error(ex, "Error in Selenium driver");
+                    _seeker = await ReopenSeeker(_seeker);
+                }
+                catch (InvalidOperationException ioe) when (ioe.Message.Contains("was not read"))
+                {
+                    _logger.Error(ioe, "Error while exploring ekw");
+                    _seeker = await ReopenSeeker(_seeker);
+                }
+                
+                _logger.Debug("Downloaded books: {DownloadedBooks}", downloadedBooks);
+            }
+        }
+        
+        private async Task ExploringStep()
+        {
+            await Task.Delay(GetRandomDelay());
+            
+            var randomBook = await _booksRepository.GetRandomNotFilledBookAsync();
+            
+            await Task.Delay(GetRandomDelay());
+            
+            var bookExists = _seeker.ReadBookInfo(randomBook);
+            
+            if (!bookExists)
+            {
+                BookInfo.MarkAsNotFound(randomBook);
+            }
+            else
+            {
+                if (randomBook?.BookType == null)
+                {
+                    throw new InvalidOperationException(
+                        "something went wrong, book properties was not read");
+                }
+                
+                if (randomBook.ClosureDate.Length <= 4)
+                {
+                    var properties = _seeker.ReadProperties();
+                    randomBook.AddNewProperties(properties);
+                }
+            }
+
+            await _booksRepository.UpdateBookAsync(randomBook);
+            await _booksRepository.AddPropertyFromBookAsync(randomBook);
+                    
+            await Task.Delay(GetRandomDelay());
+
+            _seeker.BackToCriteria();
+        }
+        
+        private int GetRandomDelay() 
+            => _random.Next(800, 2000);
+        
+        private static Task<BookInfoSeeker> OpenSeeker()
+            => ReopenSeeker(null);
+        
+        private static async Task<BookInfoSeeker> ReopenSeeker(BookInfoSeeker seeker)
+        {
+            seeker?.Clicker.Dispose();
+            
+            var clicker = await CreateClicker(ChromeOptions);
+            
+            seeker = new BookInfoSeeker(clicker);
+            return seeker;
+        }
+        
+        private static async Task<IClicker> CreateClicker(ChromeOptionsProvider optionsProvider)
+        {
+            var clicker = new SeleniumClicker(optionsProvider.Get());
+            
+            clicker.GotoHome();
+            await Task.Delay(1000);
+            clicker.CloseCookiesInfo();
+            
+            return clicker;
+        }
+    }
+}
