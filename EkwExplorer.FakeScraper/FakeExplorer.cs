458db2035be9e6237dfd8d17acb1697b7879051d
maszrum <grzyb3k@hotmail.com>
2020-11-28 18:59:00 +0100

    added FakeExplorer class

---
+++ EkwExplorer.FakeScraper/FakeExplorer.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using EkwExplorer.Core;
+using Serilog;
+
+namespace EkwExplorer.FakeScraper
+{
+    public class FakeExplorer : IBooksExplorer
+    {
+        private readonly ILogger _logger;
+        private readonly IBooksRepository _booksRepository;
+
+        public FakeExplorer(ILogger logger, IBooksRepository booksRepository)
+        {
+            _logger = logger;
+            _booksRepository = booksRepository;
+        }
+
+        public Task Explore(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
