3a6d16b5c7dbbedd10e1a0112ad04d8c8625e625
maszrum <grzyb3k@hotmail.com>
2020-11-29 14:30:59 +0100

    created element weighted randomizer

---
+++ EkwExplorer.FakeScraper/WeightedRandomizer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EkwExplorer.FakeScraper
+{
+    internal class WeightedRandomizer<T>
+    {
+        private readonly List<WeightedElement> _elements = new List<WeightedElement>();
+        private readonly Random _random = new Random(Guid.NewGuid().GetHashCode());
+
+        private int _weightsSum;
+
+        public WeightedRandomizer<T> AddElement(T element, int weight)
+        {
+            if (weight <= 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(weight), "must be positive number");
+            }
+
+            _elements.Add(new WeightedElement(element, weight));
+            _weightsSum += weight;
+
+            return this;
+        }
+
+        public T Next()
+        {
+            var randomValue = _random.Next(_weightsSum);
+
+            foreach (var element in _elements)
+            {
+                randomValue -= element.Weight;
+
+                if (randomValue < 0)
+                {
+                    return element.Element;
+                }
+            }
+
+            throw new InvalidOperationException(
+                "random element was not found");
+        }
+
+        private class WeightedElement
+        {
+            public WeightedElement(T element, int weight)
+            {
+                Element = element;
+                Weight = weight;
+            }
+
+            public T Element { get; }
+            public int Weight { get; }
+        }
+    }
+}
