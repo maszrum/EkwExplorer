1b997e6e99992f44965eaccefa917a22ece2d159
maszrum <grzyb3k@hotmail.com>
2020-11-29 15:52:05 +0100

    added randomizer class

---
+++ EkwExplorer.FakeScraper/Randomizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EkwExplorer.FakeScraper
+{
+    internal class Randomizer<T>
+    {
+        private readonly List<T> _elements = new List<T>();
+        private readonly Random _random = new Random(Guid.NewGuid().GetHashCode());
+
+        public Randomizer<T> AddElement(T element)
+        {
+            _elements.Add(element);
+
+            return this;
+        }
+
+        public T Next()
+        {
+            if (!_elements.Any())
+            {
+                throw new InvalidOperationException(
+                    "no item has been added");
+            }
+
+            var index = _random.Next(_elements.Count);
+            return _elements[index];
+        }
+    }
+}
